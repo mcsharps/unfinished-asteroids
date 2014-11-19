7670ee3e9c3780ef977fe06302f5b6d565945fc9
mcsharps <madisonsharps@gmail.com>
2014-11-18 22:55:51 -0600

    add score hud to master

--- script/Modules/Hud.js
+++ script/Modules/Hud.js
@@ -4,14 +4,19 @@ app.game.hud = {
 
     var player = app.game.players[0];
 
-    this.renderBar(16, 16, 80, 6, player.hp / player.maxHp, "#08f");
-
+    this.renderBar(16, 16, 80, 10, player.hp / player.maxHp, "#08f");
+    this.renderScore(500, 40, 80, 50, player.score, "#08f");
   },
 
   renderBar: function(x, y, width, height, progress, color) {
 
     app.layer.fillStyle("#000").fillRect(x, y, width, height);
     app.layer.fillStyle(color).fillRect(x, y, width * progress, height);
+    
+  },
+
+  renderScore: function(x, y, width, height, score, color){
+  	app.layer.fillStyle("#fff").font("24px Arial").textAlign("left").fillText("score: " + score + "", x, y);
 
   }
 
